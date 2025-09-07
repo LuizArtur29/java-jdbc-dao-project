58b5c947033f44c64aa051f24f7efee3bd89f90f
LuizArtur29 <luizarmartinsro99@gmail.com>
2025-09-07 11:16:15 -0300

    Department entity class

---
+++ src/application.java
@@ -0,0 +1,10 @@
+import model.Department;
+
+public class application {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "books");
+        System.out.println(obj.toString());
+    }
+}
