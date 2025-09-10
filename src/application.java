3f7c3a223ee68239b935deecd174c49a05eb6ad0
LuizArtur29 <luizarmartinsro99@gmail.com>
2025-09-10 10:46:14 -0300

    Add SellerDaoJDBC implementation and DaoFactory
    Introduced the SellerDaoJDBC class with method stubs for Seller DAO operations and added DaoFactory to instantiate SellerDao implementations. Updated application.java to use SellerDaoJDBC.

--- src/application.java
+++ src/application.java
@@ -1,3 +1,5 @@
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class application {
         Department obj = new Department(1, "books");
 
         Seller seller = new Seller(21, 3000.0, new Date(), "bob@gmail.com", "Bob", obj);
+
+        SellerDao sellerDao = new SellerDaoJDBC();
 
         System.out.println(seller);
     }
